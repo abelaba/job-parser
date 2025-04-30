52c0490aed9f2f678c7fb276d49658e6b0e08248
abelaba <se.abel.abate@gmail.com>
2025-05-01 00:43:51 +0300

    feat: Implement services for getting stats and streak

--- backend/internal/service/notionService.go
+++ backend/internal/service/notionService.go
@@ -221,3 +221,143 @@ func UpdateJob(pageId string) error {
 }
 
 
+func GetStats(dateRange string) (*model.StatsResult, error) {
+	databaseID := os.Getenv("NOTION_DATABASE_ID")
+	url := fmt.Sprintf("databases/%s/query", databaseID)
+
+	var dateFilter map[string]any
+	switch dateRange {
+	case "PASTYEAR":
+		dateFilter = map[string]any{"past_year": map[string]any{}}
+	case "PASTMONTH":
+		dateFilter = map[string]any{"past_month": map[string]any{}}
+	case "PASTWEEK":
+		dateFilter = map[string]any{"past_week": map[string]any{}}
+	default:
+		dateFilter = map[string]any{"past_year": map[string]any{}}
+	}
+
+	body := map[string]any{
+		"filter": map[string]any{
+			"property": "Created Date",
+			"date":     dateFilter,
+		},
+	}
+
+	var notionResponse model.NotionResponse
+
+	err := notionRequestWrapper("POST", url, body, &notionResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	stats := &model.StatsResult{
+		StatusCount:  make(map[string]int),
+		CompanyCount: make(map[string]int),
+		CountryCount: make(map[string]int),
+	}
+
+	for _, data := range notionResponse.Results {
+		status := data.Properties.Status.Status.Name
+		company := data.Properties.Company.Select.Name
+		country := data.Properties.Country.Select.Name
+
+		if status != "" {
+			stats.StatusCount[status]++
+		}
+		if company != "" {
+			stats.CompanyCount[company]++
+		}
+		if country != "" {
+			stats.CountryCount[country]++
+		}
+
+	}
+
+	return stats, nil
+
+}
+
+func GetStreak() (*model.StreakStats, error) {
+	databaseID := os.Getenv("NOTION_DATABASE_ID")
+	url := "databases/" + databaseID + "/query"
+
+	body := map[string]any{
+		"sorts": []map[string]any{
+			{
+				"property":  "Applied Date",
+				"direction": "descending",
+			},
+		},
+		"filter": map[string]any{
+			"property": "Applied Date",
+			"date": map[string]any{
+				"is_not_empty": true,
+			},
+		},
+	}
+
+	var notionResponse model.NotionResponse
+
+	err := notionRequestWrapper("POST", url, body, &notionResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var dates []time.Time
+	for _, res := range notionResponse.Results {
+		if res.Properties.AppliedDate.Date.Start != "" {
+			parsed, err := time.Parse(time.RFC3339, res.Properties.AppliedDate.Date.Start)
+			if err == nil {
+				dates = append(dates, parsed)
+			}
+		}
+	}
+
+	if len(dates) == 0 {
+		return &model.StreakStats{MaxStreak: 0, CurrentStreak: 0, LastAppliedDate: ""}, nil
+	}
+
+	maxStreak := 1
+	currentStreak := 1
+
+	for i := 1; i < len(dates); i++ {
+		prev := dates[i-1]
+		curr := dates[i]
+		diffDays := int(prev.Sub(curr).Hours() / 24)
+		if diffDays == 1 {
+			currentStreak++
+			if currentStreak > maxStreak {
+				maxStreak = currentStreak
+			}
+		} else {
+			currentStreak = 1
+		}
+	}
+
+	realCurrentStreak := 0
+	today := time.Now().Truncate(24 * time.Hour)
+	currentDate := today
+
+	for i, d := range dates {
+		d = d.Truncate(24 * time.Hour)
+		diffDays := int(currentDate.Sub(d).Hours() / 24)
+		if i == 0 && diffDays == 0 {
+			realCurrentStreak++
+		} else if i != 0 && diffDays == 1 {
+			realCurrentStreak++
+			currentDate = d
+		} else {
+			break
+		}
+		currentDate = d
+	}
+
+	return &model.StreakStats{
+		MaxStreak:       maxStreak,
+		CurrentStreak:   realCurrentStreak,
+		LastAppliedDate: dates[0].Format("2006-01-02"),
+	}, nil
+}
