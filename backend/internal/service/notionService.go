860d676688357fd5f5a30d28ea5569e5be1c456e
abelaba <se.abel.abate@gmail.com>
2025-05-01 00:41:14 +0300

    feat: Implement services for checking job posting existence
    *Service for saving job posting
    *Service for updating job posting

--- backend/internal/service/notionService.go
+++ backend/internal/service/notionService.go
@@ -95,3 +95,129 @@ func GetRecentlySavedJobs() ([]model.Job, error) {
 	return recentJobs, nil
 
 }
+
+func CheckIfJobPostingExists(jobPostingUrl string) error {
+	notionDatabaseId := os.Getenv("NOTION_DATABASE_ID")
+	url := fmt.Sprintf("databases/%v/query", notionDatabaseId)
+
+	body := map[string]any{
+		"filter": map[string]any{
+			"property": "URL",
+			"url": map[string]string{
+				"equals": jobPostingUrl,
+			},
+		},
+	}
+
+	var notionResponse model.NotionResponse
+
+	if err := notionRequestWrapper("POST", url, body, &notionResponse); err != nil {
+		return err
+	}
+
+	if(notionResponse.Results != nil && len(notionResponse.Results) > 0){
+		return errors.New("You have already applied to this position")
+	}
+
+	return nil
+}
+
+func SaveJobPosting(data *model.Job) (*model.Job, error) {
+	url:= "pages"
+	notionDatabaseId := os.Getenv("NOTION_DATABASE_ID")
+
+	body := map[string]any{
+		"parent": map[string]any{
+			"database_id": notionDatabaseId,
+		},
+		"properties": map[string]any{
+			"Link": map[string]any{
+				"title": []map[string]any{
+					{
+						"text": map[string]any{
+							"content": data.Title,
+							"link": map[string]any{
+								"url": data.URL,
+							},
+						},
+					},
+				},
+			},
+			"Status": map[string]any{
+				"status": map[string]any{
+					"name": "Not Applied",
+				},
+			},
+			"Country": map[string]any{
+				"select": map[string]any{
+					"name": data.Country,
+				},
+			},
+			"Company": map[string]any{
+				"select": map[string]any{
+					"name": data.Company,
+				},
+			},
+			"URL": map[string]any{
+				"url": data.URL,
+			},
+			"Description": map[string]any{
+				"rich_text": []map[string]any{
+					{
+						"text": map[string]any{
+							"content": data.Description,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	var notionProperties model.NotionPage
+
+	err := notionRequestWrapper("POST", url, body, &notionProperties)
+
+	if err != nil {
+		return nil, err
+	}
+
+	savedJob := &model.Job{
+			Country:     notionProperties.Properties.Country.Select.Name,
+			Company:     notionProperties.Properties.Company.Select.Name,
+			URL:         notionProperties.Properties.URL.URL,
+			Title:       notionProperties.Properties.Link.Title[0].PlainText,
+			Description: notionProperties.Properties.Description.RichText[0].PlainText,
+	}
+
+	return savedJob, nil
+
+}
+
+func UpdateJob(pageId string) error {
+	url := "pages/" + pageId
+	today := time.Now()
+	body := map[string]any{
+		"properties": map[string]any{
+			"Status": map[string]any{
+				"status": map[string]any{
+					"name": "Applied",
+				},
+			},
+			"Applied Date": map[string]any{
+				"date": map[string]any{
+					"start": today.Format(time.RFC3339), // Equivalent to toISOString()
+				},
+			},
+		},
+	}
+
+	err := notionRequestWrapper("PATCH", url, body, nil)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
