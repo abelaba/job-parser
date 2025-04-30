8bedd418fa88e7ff9f0f3194b146cd4b2fa121ae
abelaba <se.abel.abate@gmail.com>
2025-05-01 01:13:05 +0300

    feat: Implement service for saving job posting to notion

---
+++ backend/internal/service/jobService.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"job-parser-backend/internal/model"
+)
+
+func SaveJob(job model.Job) (*model.Job, error) {
+	err := CheckIfJobPostingExists(job.URL)
+	if err != nil {	
+		return nil, err
+	}
+
+	res, err := FormatDataToJSON(job.Description)
+	if err != nil {
+		return nil,  err
+	}
+
+	parsedJob := &model.Job{
+		URL:        job.URL,
+		Description: res.Description,
+		Company:    res.Company,
+		Country:    res.Country,
+		Title:      res.Title,
+	}
+
+	savedJob, err := SaveJobPosting(parsedJob)
+	if err != nil {
+		return nil, err
+	}
+
+	return savedJob, nil
+}
+
